826b3d777c7e6ad9bc7ebf133aa6f8020281a7e6
nwokporo <nwokporochukwuebuka@gmail.com>
2022-05-07 10:39:15 +0100

    Override the update method for the event api

--- django_api_project/settings.py
+++ django_api_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$=)8eviipt0o&y_+pgah85$yj^(7t5l5u1+##%e3etymyq=ti'
+SECRET_KEY = config('SK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
